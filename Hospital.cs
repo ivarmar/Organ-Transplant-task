5af8322823af4406df5e749ee9e2d8e404fd3030
ivarmar <141323294+ivarmar@users.noreply.github.com>
2023-12-05 13:11:05 +0100

    Add project files.

---
+++ Hospital.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+internal class Hospital
+{
+    private int SurvivalRate { get; set; }
+    private Random random = new Random();
+
+    public Hospital(int survivalRate)
+    {
+        SurvivalRate = survivalRate;
+        random = new Random();
+    }
+
+    public void GenerateSurvival(string userInput)
+    {
+        var percentageSurvive = RandomSurvival(0, 101);
+
+
+        if (userInput == "1")
+        {
+            percentageSurvive = RandomSurvival(40, 101);
+            Console.WriteLine($"You have given Bernt your kidney; he has {percentageSurvive}% chance to survive.");
+            Console.WriteLine($"Bernt {(percentageSurvive > 50 ? "survived" : "did not survive")}");
+
+        }
+        else if (userInput == "2")
+        {
+            percentageSurvive = RandomSurvival(0, 55);
+            Console.WriteLine($"You have refused to give your kidney. Bernt has {percentageSurvive}% chance to survive.");
+            Console.WriteLine($"Bernt {(percentageSurvive > 50 ? "survived" : "did not survive")}");
+        }
+        else
+        {
+            percentageSurvive = RandomSurvival( 0, 75);
+            Console.WriteLine($"The doctors are scrambling to find someone else to donate their kidney. Bernt has {percentageSurvive}% chance.");
+            Console.WriteLine($"Bernt {(percentageSurvive > 50 ? "survived" : "did not survive")}");
+
+        }
+    }
+
+    private int RandomSurvival(int min, int max)
+    {
+        return random.Next(min, max);
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Organ_Transplant
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var random = new Random();
+            string userInput;
+
+            while (true)
+            {
+                Console.WriteLine("There has been an accident; Bernt needs an organ transplant!");
+                Console.WriteLine(
+                    "Choose one option:\n1. Give him your kidney\n2. Refuse, your organs are your own damnit!\n3. Tell them to find someone else.");
+
+                userInput = Console.ReadLine();
+
+                if (userInput == "1" || userInput == "2" || userInput == "3")
+                {
+                    var victim = new Hospital(random.Next(1, 101));
+                    victim.GenerateSurvival(userInput);
+                }
+            }
+        }
+    }
+}
